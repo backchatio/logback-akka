079c8fd1527f79ad43ad83a07e31befb0f6d1972
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-08-31 22:30:16 +0100

    Updates dependencies that upgraded

--- project/build.scala
+++ project/build.scala
@@ -80,13 +80,13 @@ object LogbackAkkaSettings {
         "org.scalatra" %% "scalatra" % "2.0.0-SNAPSHOT" % "provided",
         "org.glassfish" % "javax.servlet" % "3.1" % "provided",
         "com.ning" % "async-http-client" % "1.6.4",
-        "org.scala-tools.time" % "time_2.9.0-1" % "0.4",
+        "org.scala-tools.time" %% "time" % "0.5",
         "se.scalablesolutions.akka" % "akka-stm" % "1.2-RC3",
         "org.slf4j" % "slf4j-api" % "1.6.1",
         "com.weiglewilczek.slf4s" % "slf4s_2.9.0-1" % "1.0.6",
         "ch.qos.logback" % "logback-classic" % "0.9.29",
         "redis.clients" % "jedis" % "1.5.2" % "provided",
-        "org.specs2" % "specs2_2.9.1.RC4" % "1.5" % "test"
+        "org.specs2" %% "specs2" % "1.6-SNAPSHOT" % "test"
       ),
       libraryDependencies ++= compilerPlugins,
       autoCompilerPlugins := true,
