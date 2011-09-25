61bac272223819e699423e54e05ea45febcba688
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-09-25 18:34:57 +0100

    be nicer

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
+
 object PluginDef extends Build {
-    override lazy val projects = Seq(root)
-    lazy val root = Project("plugins", file(".")) dependsOn( webPlugin )
-    lazy val webPlugin = uri("git://github.com/typesafehub/sbt-scalariform")
+    override lazy val projects = super.projects ++ Seq(root)
+    lazy val root = Project("plugins", file(".")) dependsOn( scalariform )
+    lazy val scalariform = uri("git://github.com/typesafehub/sbt-scalariform")
 }
